f2167b595f8a4c0064dbb57595affc98aec52895
Cubex332 <167002605+Cubex332@users.noreply.github.com>
2026-02-21 23:34:35 +0500

    UPD

--- Cassa.xaml.cs
+++ Cassa.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BakeryApp.Models;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SP1
 {
@@ -20,7 +21,6 @@ namespace SP1
 
             foreach (var item in dbContext.Products)
             {
-                int quantity = 0;
                 int prise = 0;
 
                 selectedItems[item.Id] = (quantity: 0, price: (int)item.Price, Id: item.Id, prise: 0);
@@ -30,6 +30,9 @@ namespace SP1
                     Padding = new Thickness(10),
                     HorizontalOptions = LayoutOptions.Center
                 };
+                Label? chooseText = null;
+                Button? editquantityButton = null;
+                Button? deletequantityButton = null;
 
                 var button = new Button
                 {
@@ -38,28 +41,96 @@ namespace SP1
                     BackgroundColor = Colors.LightGray,
                 };
 
-                Label chooseText = null;
 
                 button.Clicked += async (sender, e) =>
                 {
-                    quantity++;
-                    prise += (int)item.Price;
-
-                    selectedItems[item.Id] = (quantity, (int)item.Price, item.Id, prise);
+                    prise = (int)item.Price;
 
                     if (chooseText == null)
                     {
-                        chooseText = new Label
+                        string qualityTextInput = await DisplayPromptAsync(
+                             title: $"Действия для товара '{item.Name}'",
+                             message: null,
+                             accept: "Добавить",
+                             cancel: "Отмена",
+                             placeholder: "Количество",
+                             keyboard: Keyboard.Numeric
+                        );
+                        if (!string.IsNullOrWhiteSpace(qualityTextInput))
                         {
-                            Text = $"Вы выбрали {item.Name} за {prise} ₸ в количестве {quantity} шт.",
-                            FontSize = 16,
-                            Margin = new Thickness(0, 2)
-                        };
-                        MainLayout.Children.Add(chooseText);
+                            if(int.TryParse(qualityTextInput, out var quality) && quality > 0)
+                            {
+                                var horizontalGroup = new HorizontalStackLayout
+                                {
+                                    Spacing = 20
+                                };
+
+                                prise *= quality;
+                                chooseText = new Label
+                                {
+                                    Text = $"Вы выбрали {item.Name} за {prise} ₸ в количестве {quality} шт.",
+                                    FontSize = 16,
+                                    Margin = new Thickness(0, 2)
+                                };
+                                editquantityButton = new Button
+                                {
+                                    Text = "✏️",
+                                    FontSize = 16
+                                };
+                                deletequantityButton = new Button
+                                {
+                                    Text = "❌",
+                                    FontSize = 16
+                                };
+
+                                deletequantityButton.Clicked += async (sender, e) =>
+                                {
+                                    var result = await DisplayAlertAsync("Внимание", "Вы действительно хотите удалить?", "Да", "Нет");
+                                    if (result)
+                                    {
+                                        selectedItems.Remove(item.Id);
+                                        MainLayout.Children.Remove(horizontalGroup);
+                                        chooseText = null;
+                                        editquantityButton = null;
+                                        deletequantityButton = null;
+                                    }
+                                };
+
+                                editquantityButton.Clicked += async (sender, e) =>
+                                {
+                                    string EditqualityTextInput = await DisplayPromptAsync(
+                                        title: $"Действия для товара '{item.Name}'",
+                                        message: null,
+                                        accept: "Добавить",
+                                        cancel: "Отмена",
+                                        placeholder: "Введите новое колисчество",
+                                        keyboard: Keyboard.Numeric
+                                    );
+
+                                    if (!string.IsNullOrWhiteSpace(EditqualityTextInput))
+                                    {
+                                        if (int.TryParse(EditqualityTextInput, out var Editquality) && Editquality > 0)
+                                        {
+                                            var newprise = (int)item.Price * Editquality;
+                                            chooseText.Text = $"Вы выбрали {item.Name} за {newprise} ₸ в количестве {Editquality} шт.";
+                                            selectedItems[item.Id] = (Editquality, (int)item.Price, item.Id, newprise);
+                                            await DisplayAlertAsync("Успешно", "Количество товара изменено", "Ok");
+                                        }
+                                    }
+                                };
+
+                                horizontalGroup.Children.Add(chooseText);
+                                horizontalGroup.Children.Add(editquantityButton);
+                                horizontalGroup.Children.Add(deletequantityButton);
+                                MainLayout.Children.Add(horizontalGroup);
+
+                                selectedItems[item.Id] = (quality, (int)item.Price, item.Id, prise);
+                            }
+                        }
                     }
                     else
                     {
-                        chooseText.Text = $"Вы выбрали {item.Name} за {prise} ₸ в количестве {quantity} шт.";
+                        await DisplayAlertAsync("Error", $"Товар {item.Name} уже добавлен", "Ok");
                     }
                 };
 
@@ -67,56 +138,58 @@ namespace SP1
                 FlexLayoutContainer.Children.Add(vertical);
             }
 
-            Calculate.Clicked += async (sender, e) =>
+            Calculate.Clicked += async(sender, e) => await OnCalculate(sender, e);
+        }
+
+        private async Task OnCalculate(object? sender, EventArgs e)
+        {
+            try
             {
-                try
+                var itemsToOrder = selectedItems.Values
+                    .Where(x => x.quantity > 0)
+                    .ToList();
+
+                if (!itemsToOrder.Any())
                 {
-                    var itemsToOrder = selectedItems.Values
-                        .Where(x => x.quantity > 0)
-                        .ToList();
-
-                    if (!itemsToOrder.Any())
-                    {
-                        await DisplayAlert("Внимание", "Выберите товары", "Ok");
-                        return;
-                    }
-
-                    var customer = dbContext.Customers.ToList().FirstOrDefault();
-                    var employee = dbContext.Employees.ToList().FirstOrDefault();
-
-                    if (customer == null || employee == null)
-                    {
-                        await DisplayAlert("Ошибка", "Клиент или сотрудник не найден", "Ok");
-                        return;
-                    }
-
-                    var newOrder = new Order
-                    {
-                        CustomerId = customer.Id,
-                        EmployeeId = employee.Id
-                    };
-                    dbContext.Orders.Add(newOrder);
-                    await dbContext.SaveChangesAsync();
-
-                    foreach (var item in itemsToOrder)
-                    {
-                        dbContext.OrderItems.Add(new OrderItem
-                        {
-                            OrderId = newOrder.Id,
-                            ProductId = item.Id,
-                            Quantity = item.quantity,
-                            Price = item.prise
-                        });
-                    }
-
-                    await dbContext.SaveChangesAsync();
-                    await DisplayAlert("Успех", "Заказ оформлен!", "Ok");
+                    await DisplayAlertAsync("Внимание", "Выберите товары", "Ok");
+                    return;
                 }
-                catch (Exception ex)
+
+                var customer = dbContext.Customers.ToList().FirstOrDefault();
+                var employee = dbContext.Employees.ToList().FirstOrDefault();
+
+                if (customer == null || employee == null)
                 {
-                    await DisplayAlert("Error", ex.InnerException?.Message ?? ex.Message, "Ok");
+                    await DisplayAlertAsync("Ошибка", "Клиент или сотрудник не найден", "Ok");
+                    return;
                 }
-            };
+
+                var newOrder = new Order
+                {
+                    CustomerId = customer.Id,
+                    EmployeeId = employee.Id
+                };
+                dbContext.Orders.Add(newOrder);
+                await dbContext.SaveChangesAsync();
+
+                foreach (var item in itemsToOrder)
+                {
+                    dbContext.OrderItems.Add(new OrderItem
+                    {
+                        OrderId = newOrder.Id,
+                        ProductId = item.Id,
+                        Quantity = item.quantity,
+                        Price = item.prise
+                    });
+                }
+
+                await dbContext.SaveChangesAsync();
+                await DisplayAlertAsync("Успех", "Заказ оформлен!", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Ошибка", ex.InnerException?.Message ?? ex.Message, "Ok");
+            }
         }
     }
 }
